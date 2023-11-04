296b503e4c1029a702f7f3b7ee620ec19322904c
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-11-04 15:14:56 +0900

    Add BiomeJS linter and formatter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+import path from "node:path";
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+// NG: top-level await
+// await sleep(0)
+
+const main = async () => {
+    path.isAbsolute('/') && await sleep(0);
+    console.log("Hello, world!")
+}
+
+main();
